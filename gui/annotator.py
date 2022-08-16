f236dba6659d2c1960c4201f416eb9d622d4f839
OHuynh <olivierv.huynh@gmail.com>
2022-08-16 12:41:17 +0200

    visualize current annotation and add more distinct colors

--- gui/annotator.py
+++ gui/annotator.py
@@ -2,8 +2,10 @@ import cv2
 import numpy as np
 from functools import partial
 
-from gui.labeler import Labeler, LabelerAction
+from utils.distinct_colors import COLORS
 
+from gui.labeler import Labeler, LabelerAction
+from data.sequence_bound import SequenceBound
 
 class Annotator:
     """
@@ -33,10 +35,9 @@ class Annotator:
         self._type_trajectory = None
         self._sequence_bb = []
 
-        max_obj_per_video = 500
-        np.random.seed(0)
-        self.color_id = np.random.randint(low=0, high=255, size=(3, max_obj_per_video))
+        self.color_id = np.array(COLORS, dtype=np.uint8)
         self.font = cv2.FONT_HERSHEY_PLAIN
+        self.color_annotate = (0, 255, 0)
 
     def init_window(self):
         # init the window
@@ -58,10 +59,23 @@ class Annotator:
             prev_frame_idx = frame_idx
             if self._ret:
                 frame_to_show = self._frame.copy()
-                self.display_chunk_seq(frame_idx, frame_to_show)
+
+                for obj_id in self._database.database:
+                    color = self.color_id[obj_id, :].tolist()
+                    label = f'{obj_id} {Labeler.classes[self._database.database[obj_id][0]]}'
+                    for sequence in self._database.database[obj_id][1]:
+                        self.display_chunk_seq(sequence, frame_idx, frame_to_show, color, label)
+
+                if len(self._sequence_bb) > 0:
+                    # add boundaries rect to build a valid sequence to show
+                    tmp_sequence_bb = self._sequence_bb.copy()
+                    tmp_sequence_bb.insert(0,  (0, (0, 0), (0, 0), 0))
+                    tmp_sequence_bb.append((self._nb_frames, (0, 0), (0, 0), 0))
+                    tmp_sequence = SequenceBound(tmp_sequence_bb)
+                    self.display_chunk_seq(tmp_sequence, frame_idx, frame_to_show, self.color_annotate, '')
 
                 if self._drawing_rect:
-                    cv2.rectangle(frame_to_show, self._top_left, self._bottom_right, (0, 255, 0), 2)
+                    cv2.rectangle(frame_to_show, self._top_left, self._bottom_right, self.color_annotate, 2)
                 if self._rect_drawn:
                     self._rect_drawn = False
                     show_rect = False
@@ -89,7 +103,7 @@ class Annotator:
                         self._sequence_bb.append((frame_idx, self._top_left, self._bottom_right, None))
                         show_rect = True
                     if show_rect:
-                        cv2.rectangle(frame_to_show, self._top_left, self._bottom_right, (0, 255, 0), 2)
+                        cv2.rectangle(frame_to_show, self._top_left, self._bottom_right, self.color_annotate, 2)
                         self._frame = frame_to_show
 
                 cv2.imshow(self._window_name, frame_to_show)
@@ -101,32 +115,28 @@ class Annotator:
                 elif c == ord('-') and frame_idx > 1:
                     cv2.setTrackbarPos(self._trackbar_name, self._window_name, frame_idx - 1)
 
-    def display_chunk_seq(self, frame_idx, frame_to_show):
-        for obj_id in self._database.database:
-            for sequence in self._database.database[obj_id][1]:
-                for chunk_idx in range(len(sequence.time_markers) - 1):
-                    if sequence.time_markers[chunk_idx] <= frame_idx <= sequence.time_markers[chunk_idx + 1]:
-                        thickness = 2 if sequence.time_markers[chunk_idx] == frame_idx else 1
-                        color = self.color_id[:, obj_id].tolist()
-                        label = f'{obj_id} {Labeler.classes[self._database.database[obj_id][0]]}'
-                        cv2.rectangle(frame_to_show,
-                                      sequence.bb[chunk_idx][0],
-                                      sequence.bb[chunk_idx][1],
-                                      color, thickness)
-                        cv2.putText(frame_to_show,
-                                    label,
-                                    (sequence.bb[chunk_idx][0][0], sequence.bb[chunk_idx][0][1] + 30),
-                                    self.font, 1, color, 1)
+    def display_chunk_seq(self, sequence, frame_idx, frame_to_show, color, label):
+        for chunk_idx in range(len(sequence.time_markers) - 1):
+            if sequence.time_markers[chunk_idx] <= frame_idx <= sequence.time_markers[chunk_idx + 1]:
+                thickness = 2 if sequence.time_markers[chunk_idx] == frame_idx else 1
+                cv2.rectangle(frame_to_show,
+                              sequence.bb[chunk_idx][0],
+                              sequence.bb[chunk_idx][1],
+                              color, thickness)
+                cv2.putText(frame_to_show,
+                            label,
+                            (sequence.bb[chunk_idx][0][0], sequence.bb[chunk_idx][0][1] + 30),
+                            self.font, 1, color, 1)
 
-                        thickness = 2 if sequence.time_markers[chunk_idx + 1] == frame_idx else 1
-                        cv2.rectangle(frame_to_show,
-                                      sequence.bb[chunk_idx + 1][0],
-                                      sequence.bb[chunk_idx + 1][1],
-                                      color, thickness)
-                        cv2.putText(frame_to_show,
-                                    label,
-                                    (sequence.bb[chunk_idx + 1][0][0], sequence.bb[chunk_idx + 1][0][1] + 30),
-                                    self.font, 1, color, 1)
+                thickness = 2 if sequence.time_markers[chunk_idx + 1] == frame_idx else 1
+                cv2.rectangle(frame_to_show,
+                              sequence.bb[chunk_idx + 1][0],
+                              sequence.bb[chunk_idx + 1][1],
+                              color, thickness)
+                cv2.putText(frame_to_show,
+                            label,
+                            (sequence.bb[chunk_idx + 1][0][0], sequence.bb[chunk_idx + 1][0][1] + 30),
+                            self.font, 1, color, 1)
         return frame_to_show
 
     def label_emitter(self, labeler_action, label, obj_id, type_trajectory):
